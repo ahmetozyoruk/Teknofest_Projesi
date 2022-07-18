3ba64a808f5745b576e3f72634da50f488ad0d6a
ahmetozyoruk <ahmetozyoruk3@hotmail.com>
2022-07-17 20:58:35 -0400

    hotspot eklendi

--- Uygulama/AppTeknofest/app/src/main/java/com/example/appteknofest/MainActivity.java
+++ Uygulama/AppTeknofest/app/src/main/java/com/example/appteknofest/MainActivity.java
@@ -1,17 +1,34 @@
 package com.example.appteknofest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.Navigation;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements APManager.OnSuccessListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.btnTurnOn).setOnClickListener(v -> {
+            APManager apManager = APManager.getApManager(this);
+            apManager.turnOnHotspot(this,
+                    this,
+                    new DefaultFailureListener(this)
+            );
 
+        });
+    }
+
+    @Override
+    public void onSuccess(@NonNull String ssid, @NonNull String password) {
+        Toast.makeText(this, ssid + "," + password, Toast.LENGTH_LONG).show();
+        System.out.println("password: "+ password);
+        startActivity(new Intent(this, Introductionragment.class));
     }
 }
